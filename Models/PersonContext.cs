ac244fd54866c8ba3e32b8b769719090ac6497dd
Venkateswarlu Kanchanam <Venkateswarlu.Kanchanam_HCL@kantar.com>
2020-04-04 17:07:09 +0530

    Add project files.

---
+++ Models/PersonContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.Models
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options) : base(options) { }
+       
+        public DbSet<Person> People { get; set; }
+        
+    }
+} 
+
